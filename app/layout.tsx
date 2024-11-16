bf5937e73c8f72a6a8f522dfde78166ddf16bb9d
artemsvit <a.svitelskyi@gmail.com>
2024-11-16 12:51:41 +0200

    Update layout and theme toggle components

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning className="dark">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
         </ThemeProvider>
       </body>
